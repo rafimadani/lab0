bf7c4311c8bbb3a87f5d86f49d9d5f0d57e714a4
Rafi Madani <rafimadani@Rafis-MacBook-Pro.local>
2022-09-22 00:33:38 +0700

    untuk tutorial lab3

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,12 +3,20 @@ from wishlist.views import show_wishlist
 from wishlist.views import show_xml #sesuaikan dengan nama fungsi yang dibuat
 from wishlist.views import show_json #sesuaikan dengan nama fungsi yang dibuat
 from wishlist.views import show_json_by_id #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import register 
+from wishlist.views import login_user #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import logout_user #sesuaikan dengan nama fungsi yang dibuat
+
 app_name = 'wishlist'
+
 
 urlpatterns = [
     path('wishlist/', show_wishlist, name='show_wishlist'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'), #sesuaikan dengan nama fungsi yang dibuat
-
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'), #backup
+    path('wishlist/login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
